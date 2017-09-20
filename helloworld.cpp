a76c5ce751d80546a1c5395d0a3f7f9ed4a4e400
Eric Y <ericliuyang@gmail.com>
2017-09-20 12:44:17 -0700

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello, world." << endl;
+	cout << "Hello, world." << endl;	//Some comment
+	cout << "I'm good. Thanks!" << endl;	//yet another comment!
 	return 0;
 }
